fcad123f664d968881ac59f486c2b3a7f69d82b8
Pham Anh Dung <anhdungpham090@gmail.com>
2022-12-04 21:08:18 +0700

    Test improve Perf.

--- src/server/trpc/router/item.ts
+++ src/server/trpc/router/item.ts
@@ -5,86 +5,129 @@ export const itemRouter = router({
 	getAll: publicProcedure.input(searchQueryValidator).query(async ({ ctx, input }) => {
 		const { search, sortBy, direction, color, itemCategory, page, limit, cursor } = input;
 
-		const [totalRecord, records] = await ctx.prisma.$transaction([
-			ctx.prisma.item.count({
-				where: {
-					...(search
-						? {
-								name: {
-									contains: search,
-									mode: 'insensitive',
-								},
-						  }
-						: {}),
-					AND: [
-						...(itemCategory
-							? [
-									{
-										itemCategories: {
-											some: {
-												name: {
-													equals: itemCategory,
-												},
+		// const [totalRecord, records] = await ctx.prisma.$transaction([
+		// 	ctx.prisma.item.count({
+		// 		where: {
+		// 			...(search
+		// 				? {
+		// 						name: {
+		// 							contains: search,
+		// 							mode: 'insensitive',
+		// 						},
+		// 				  }
+		// 				: {}),
+		// 			AND: [
+		// 				...(itemCategory
+		// 					? [
+		// 							{
+		// 								itemCategories: {
+		// 									some: {
+		// 										name: {
+		// 											equals: itemCategory,
+		// 										},
+		// 									},
+		// 								},
+		// 							},
+		// 					  ]
+		// 					: []),
+		// 				...(color
+		// 					? [
+		// 							{
+		// 								color: {
+		// 									equals: color,
+		// 								},
+		// 							},
+		// 					  ]
+		// 					: []),
+		// 			],
+		// 		},
+		// 	}),
+		// 	ctx.prisma.item.findMany({
+		// 		where: {
+		// 			...(search
+		// 				? {
+		// 						name: {
+		// 							contains: search,
+		// 							mode: 'insensitive',
+		// 						},
+		// 				  }
+		// 				: {}),
+		// 			AND: [
+		// 				...(itemCategory
+		// 					? [
+		// 							{
+		// 								itemCategories: {
+		// 									some: {
+		// 										name: {
+		// 											equals: itemCategory,
+		// 										},
+		// 									},
+		// 								},
+		// 							},
+		// 					  ]
+		// 					: []),
+		// 				...(color
+		// 					? [
+		// 							{
+		// 								color: {
+		// 									equals: color,
+		// 								},
+		// 							},
+		// 					  ]
+		// 					: []),
+		// 			],
+		// 		},
+		// 		orderBy: {
+		// 			[sortBy]: direction,
+		// 		},
+		// 		skip: (page - 1) * limit,
+		// 		take: limit,
+		// 		cursor: cursor ? { id: cursor } : undefined,
+		// 	}),
+		// ]);
+
+		const records = await ctx.prisma.item.findMany({
+			where: {
+				...(search
+					? {
+							name: {
+								contains: search,
+								mode: 'insensitive',
+							},
+					  }
+					: {}),
+				AND: [
+					...(itemCategory
+						? [
+								{
+									itemCategories: {
+										some: {
+											name: {
+												equals: itemCategory,
 											},
 										},
 									},
-							  ]
-							: []),
-						...(color
-							? [
-									{
-										color: {
-											equals: color,
-										},
-									},
-							  ]
-							: []),
-					],
-				},
-			}),
-			ctx.prisma.item.findMany({
-				where: {
-					...(search
-						? {
-								name: {
-									contains: search,
-									mode: 'insensitive',
 								},
-						  }
-						: {}),
-					AND: [
-						...(itemCategory
-							? [
-									{
-										itemCategories: {
-											some: {
-												name: {
-													equals: itemCategory,
-												},
-											},
-										},
+						  ]
+						: []),
+					...(color
+						? [
+								{
+									color: {
+										equals: color,
 									},
-							  ]
-							: []),
-						...(color
-							? [
-									{
-										color: {
-											equals: color,
-										},
-									},
-							  ]
-							: []),
-					],
-				},
-				orderBy: {
-					[sortBy]: direction,
-				},
-				skip: (page - 1) * limit,
-				take: limit,
-				cursor: cursor ? { id: cursor } : undefined,
-			}),
-		]);
+								},
+						  ]
+						: []),
+				],
+			},
+			orderBy: {
+				[sortBy]: direction,
+			},
+			skip: (page - 1) * limit,
+			take: limit,
+			cursor: cursor ? { id: cursor } : undefined,
+		});
 
 		const newCursor = records.at(-1)?.id as string;
 
@@ -93,8 +136,8 @@ export const itemRouter = router({
 			cursor: newCursor,
 			page,
 			limit,
-			totalRecord,
-			totalPage: Math.ceil(totalRecord / limit),
+			// totalRecord,
+			// totalPage: Math.ceil(totalRecord / limit),
 		};
 	}),
 	// getOne: publicProcedure.query(async ({ ctx }) => {}),
