e6f09ed882cb6f2c9fd702ccea71ac5b0d5b1e4e
Pham Anh Dung <anhdungpham090@gmail.com>
2022-12-12 15:14:34 +0700

    change favicon.

--- src/components/Layout/RootLayout/index.tsx
+++ src/components/Layout/RootLayout/index.tsx
@@ -12,7 +12,7 @@ const RootLayout: FC<{ children?: ReactNode }> = ({ children }) => {
 				<title>{APP_NAME}</title>
 				<meta name='description' content={APP_DESCRIPTION} />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-				<link rel='icon' href='/favicon.jpg' />
+				<link rel='icon' href='/favicon.ico' />
 				<meta name='keywords' content={APP_KEYWORD} />
 			</Head>
 
