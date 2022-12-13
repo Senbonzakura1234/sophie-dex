1e0b05a696215035ad2fa5ab628612390dae35fc
Pham Anh Dung <anhdungpham090@gmail.com>
2022-12-13 23:10:03 +0700

    Refactor zod.

--- src/types/common/zod.ts
+++ src/types/common/zod.ts
@@ -3,22 +3,34 @@ import { z } from 'zod';
 
 import { colorMap, itemCategoryList, traitCategoryList } from '../model';
 
+const positiveIntSchema = z.string().regex(positiveIntRegex);
+
+const searchSchema = z.string().nullish().catch(null);
+const sortBySchema = z.enum(['noId', 'name']).catch('noId');
+const directionSchema = z.enum(['asc', 'desc']).catch('asc');
+const itemCategorySchema = z.enum(itemCategoryList).nullish().catch(null);
+const traitCategorySchema = z.enum(traitCategoryList).nullish().catch(null);
+const colorSchema = z.enum(colorMap).nullish().catch(null);
+const idSchema = z.string().regex(idRegex).nullish().catch(null);
+const pageSchema = positiveIntSchema.nullish().catch('1');
+const limitSchema = positiveIntSchema.nullish().catch(defaultLimit);
+
 export const searchQueryValidator = z.object({
-	search: z.string().nullish().catch(null),
-	sortBy: z.enum(['noId', 'name']).catch('noId'),
-	direction: z.enum(['asc', 'desc']).catch('asc'),
-	itemCategory: z.enum(itemCategoryList).nullish().catch(null),
-	traitCategory: z.enum(traitCategoryList).nullish().catch(null),
-	color: z.enum(colorMap).nullish().catch(null),
-	cursor: z.string().regex(idRegex).nullish().catch(null),
-	page: z.string().regex(positiveIntRegex).nullish().catch('1'),
-	limit: z.string().regex(positiveIntRegex).nullish().catch(defaultLimit),
+	search: searchSchema,
+	sortBy: sortBySchema,
+	direction: directionSchema,
+	itemCategory: itemCategorySchema,
+	traitCategory: traitCategorySchema,
+	color: colorSchema,
+	cursor: idSchema,
+	page: pageSchema,
+	limit: limitSchema,
 });
 
 export type SearchQuery = z.infer<typeof searchQueryValidator>;
 
 export const idQueryValidator = z.object({
-	id: z.string().regex(idRegex).nullish().catch(null),
+	id: idSchema,
 });
 
 export type IdQuery = z.infer<typeof idQueryValidator>;
