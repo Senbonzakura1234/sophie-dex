d9b3f29949cbf2b4ca5d743f146a1ec4fcaf9068
Pham Anh Dung <anhdungpham090@gmail.com>
2022-12-16 14:35:46 +0700

    Implement analytic.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,18 @@ import '@root/styles/globals.css';
 
 import RootLayout from '@root/components/Layout/RootLayout';
 import { trpc } from '@root/utils/trpc';
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from '@vercel/analytics/react';
 import { type AppType } from 'next/app';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<RootLayout>
-			<Component {...pageProps} />
-		</RootLayout>
+		<>
+			<RootLayout>
+				<Component {...pageProps} />
+			</RootLayout>
+			<Analytics />
+		</>
 	);
 };
 
