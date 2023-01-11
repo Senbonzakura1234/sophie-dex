29e56f6a2a7c2ddc2dc2038883225097bffe5e32
Pham Anh Dung <anhdungpham090@gmail.com>
2023-01-11 15:52:21 +0700

    Refactor code..

--- src/components/ItemRecord/Description/index.tsx
+++ src/components/ItemRecord/Description/index.tsx
@@ -1,37 +1,15 @@
 import type { DescriptionProps } from '@root/types/common/props';
 import type { FC } from 'react';
 
-const Description: FC<DescriptionProps> = ({ description: { location, hunt, rumor, shop, special } }) => {
+const Description: FC<DescriptionProps> = ({ description }) => {
 	return (
 		<ul className='max-w-fit list-disc pl-8 text-lg sm:max-w-[50%]'>
-			{location && (
-				<li>
-					<span className='inline font-bold'>Location:</span> {location}
-				</li>
-			)}
-
-			{hunt && (
-				<li>
-					<span className='inline font-bold'>Hunt:</span> {hunt}
-				</li>
-			)}
-
-			{rumor && (
-				<li>
-					<span className='inline font-bold'>Rumor:</span> {rumor}
-				</li>
-			)}
-
-			{shop && (
-				<li>
-					<span className='inline font-bold'>Shop:</span> {shop}
-				</li>
-			)}
-
-			{special && (
-				<li>
-					<span className='inline font-bold'>Special:</span> {special}
-				</li>
+			{Object.entries(description).map(([key, value]) =>
+				value ? (
+					<li key={key}>
+						<span className='inline font-bold capitalize'>{key.toLowerCase()}:</span> {value}
+					</li>
+				) : null,
 			)}
 		</ul>
 	);
