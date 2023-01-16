95464a05dd8b39140e18768c85e69613cf7a0eb0
Pham Anh Dung <anhdungpham090@gmail.com>
2023-01-16 14:38:45 +0700

    Fix css.

--- src/components/TraitRecord/TraitMergeList/index.tsx
+++ src/components/TraitRecord/TraitMergeList/index.tsx
@@ -5,7 +5,7 @@ import type { FC } from 'react';
 import { Fragment } from 'react';
 
 const TraitMergeList: FC<TraitMergeListProps> = ({ mergeFrom }) => (
-	<nav className='relative z-50 mb-3 flex flex-wrap gap-2'>
+	<nav className='relative z-50 flex flex-wrap gap-2'>
 		<h5 className={clsx({ hidden: mergeFrom.length <= 0 }, 'my-auto font-extrabold')}>Merge from:</h5>
 
 		{mergeFrom.map(({ consist }, key) => (
