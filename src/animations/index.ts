cac1c77da0f064342d55d7bb77fe1c73d9b9cc07
Pham Anh Dung <anhdungpham090@gmail.com>
2023-04-13 12:18:52 +0700

    Optimizing framer-motion.

--- src/animations/index.ts
+++ src/animations/index.ts
@@ -5,3 +5,9 @@ export const getFramerFadeUp = (delay = 0, fadeUpRange = 100, duration = 1): Ani
 	animate: { opacity: 1, y: 0, transition: { delay, duration, type: 'spring' } },
 	exit: { opacity: 0, y: fadeUpRange, transition: { delay, duration, type: 'spring' } },
 });
+
+export const getFramerFade = (delay = 0.1, duration = 2): AnimationProps => ({
+	initial: { opacity: 0 },
+	animate: { opacity: 1, transition: { delay, duration, type: 'spring' } },
+	exit: { opacity: 0, transition: { delay, duration, type: 'spring' } },
+});
