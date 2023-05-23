9809ae010c3a26a37725aecc1ba16073655809e7
Pham Anh Dung <anhdungpham090@gmail.com>
2023-05-23 08:53:48 +0700

    refactor code.

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,8 +1,14 @@
 import useDebugger from '@root/hooks/useDebugger';
 import { useSearchQuery } from '@root/hooks/useSecuredRouter';
 import { apiContext } from '@root/utils/trpc';
-import { type NextPage } from 'next';
+import type { GetServerSidePropsContext, NextPage } from 'next';
 
+export function getServerSideProps(context: GetServerSidePropsContext) {
+	context.res.statusCode = 403;
+	return {
+		props: {},
+	};
+}
 const Tests: NextPage = () => {
 	const { securedQuery, isReady } = useSearchQuery();
 	const { data, isError, error } = apiContext.test.test.useQuery(securedQuery, {
