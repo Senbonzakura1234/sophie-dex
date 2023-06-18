bf5cc073b8fd0ac05d82e29b62a9bf71b52ad6ac
Pham Anh Dung <anhdungpham090@gmail.com>
2023-06-18 23:29:07 +0700

    Implement Analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import '@total-typescript/ts-reset';
 
 import RootLayout from '@root/components/Layout/RootLayout';
 import { apiContext } from '@root/utils/trpc';
+import { Analytics } from '@vercel/analytics/react';
 import { type AppType } from 'next/app';
 
 const MyApp: AppType = ({ Component, pageProps }) => (
 	<RootLayout>
 		<Component {...pageProps} />
+		<Analytics />
 	</RootLayout>
 );
 
