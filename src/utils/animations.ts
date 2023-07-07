2f7f45cbb934544787b32ceefe34d3b5b80436bc
Pham Anh Dung <anhdungpham090@gmail.com>
2023-07-08 00:36:26 +0700

    Re-install framer motion.

--- src/utils/animations.ts
+++
@@ -1,7 +0,0 @@
-import type { MotionProps } from 'framer-motion';
-
-export const getFramerFadeUp = (delay = 0, fadeUpRange = 20, duration = 0.5): MotionProps => ({
-	initial: { opacity: 0, y: fadeUpRange },
-	animate: { opacity: 1, y: 0, transition: { delay, duration } },
-	exit: { opacity: 0, y: fadeUpRange, transition: { delay, duration } },
-});
