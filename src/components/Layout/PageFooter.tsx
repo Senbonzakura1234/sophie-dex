f341abb2cd627cbd09bcb3527fe02cf313960d77
Pham Anh Dung <anhdungpham090@gmail.com>
2023-07-12 20:25:24 +0700

    Optimize image.

--- src/components/Layout/PageFooter.tsx
+++ src/components/Layout/PageFooter.tsx
@@ -12,22 +12,22 @@ type PageFooterProps = ModuleIdProps;
 export default function PageFooter({ moduleId }: PageFooterProps) {
 	return (
 		<section className='relative min-h-[300px] w-full overflow-hidden shadow-inner'>
-			<Image
-				className='object-cover'
-				fill
-				placeholder='blur'
-				src={bottomBg}
-				alt='sophie'
-				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-				quality={10}
-				data-url={`${getBaseUrl(true)}/assets/images/bottom.jpg`}
-			/>
-
-			<div className='absolute inset-0 z-10 bg-gradient-to-r from-accent/10 to-primary/10' />
-
 			<div className='container absolute inset-0 z-20 mx-auto flex flex-wrap place-content-center gap-3 px-4 pt-5 2xl:gap-6'>
 				<Breadcrumb isShowAuthor={true} moduleId={moduleId} />
 			</div>
+
+			<div className='absolute inset-0 z-10 bg-gradient-to-r from-accent/10 to-primary/10' />
+
+			<Image
+				alt='sophie'
+				className='object-cover'
+				data-url={`${getBaseUrl(true)}/assets/images/bottom.jpg`}
+				fill
+				placeholder='blur'
+				quality={70}
+				sizes='(max-width: 768px) 100vw, 50vw'
+				src={bottomBg}
+			/>
 		</section>
 	);
 }
--- src/components/Layout/PageTitle.tsx
+++ src/components/Layout/PageTitle.tsx
@@ -15,29 +15,16 @@ export default function PageTitle({ moduleId }: PageTitleProps) {
 
 	return (
 		<section className={`relative w-full overflow-hidden shadow-inner ${isHomePage ? 'h-full' : 'min-h-[300px]'}`}>
-			<Image
-				alt='sophie'
-				className='object-cover'
-				data-url={`${getBaseUrl(true)}/assets/images/top.jpg`}
-				fill
-				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-				src={topBg}
-				placeholder='blur'
-				priority={!isHomePage}
-			/>
-
-			<div className='absolute inset-0 z-10 bg-gradient-to-r from-primary/20 to-slate-700/20' />
-
 			<div className='container absolute inset-0 z-20 mx-auto flex flex-wrap place-content-center gap-4 px-4 pt-5 xl:gap-9 2xl:gap-6'>
 				<div className='w-full text-center'>
 					{isHomePage ? (
 						<Image
-							className='mx-auto w-[700px] max-w-full'
-							src={sophieLogo}
 							alt='home'
+							className='mx-auto w-[700px] max-w-full'
+							data-url={`${getBaseUrl(true)}/assets/images/sophie-logo.png`}
 							priority={isHomePage}
 							sizes='(max-width: 768px) 70vw, (max-width: 1200px) 400px, 600px'
-							data-url={`${getBaseUrl(true)}/assets/images/sophie-logo.png`}
+							src={sophieLogo}
 						/>
 					) : (
 						<div className='font-roboto text-5xl font-bold capitalize tracking-widest text-slate-50 text-shadow-dark xl:text-6xl 2xl:text-7xl'>
@@ -47,6 +34,20 @@ export default function PageTitle({ moduleId }: PageTitleProps) {
 				</div>
 				<Breadcrumb isShowAuthor={isHomePage} moduleId={moduleId} />
 			</div>
+
+			<div className='absolute inset-0 z-10 bg-gradient-to-r from-primary/20 to-slate-700/20' />
+
+			<Image
+				alt='sophie'
+				className='object-cover'
+				data-url={`${getBaseUrl(true)}/assets/images/top.jpg`}
+				fill
+				placeholder='blur'
+				priority={!isHomePage}
+				quality={70}
+				sizes='(max-width: 768px) 100vw, 50vw'
+				src={topBg}
+			/>
 		</section>
 	);
 }
