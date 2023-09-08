3bd8ab1ff8b2e0f35585eabae78a597733ec13b6
Pham Anh Dung <anhdungpham090@gmail.com>
2023-09-08 23:52:59 +0700

    Disable edge trpc.

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,9 +1,6 @@
 import { appRouter } from '@root/server/api/router/_app';
 import { LogProvider } from '@root/utils/common';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import type { ServerRuntime } from 'next';
-
-export const runtime: ServerRuntime = 'edge';
 
 export type AppRouter = typeof appRouter;
 
