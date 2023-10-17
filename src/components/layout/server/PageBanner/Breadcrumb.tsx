dc5e5fbd26b0a8d6ce45e3bd24de3c6eaee01520
Pham Anh Dung <anhdungpham090@gmail.com>
2023-10-17 10:25:55 +0700

    refactor code.

--- src/components/layout/server/PageBanner/Breadcrumb.tsx
+++ src/components/layout/server/PageBanner/Breadcrumb.tsx
@@ -7,8 +7,8 @@ import { listAboutPaths } from '@root/constants/common';
 import usePageSegment from '@root/hooks/usePageSegment';
 import { moduleIdList } from '@root/types/model';
 import { arrayInclude } from '@root/utils/common';
+import Link from 'next/link';
 import { Fragment } from 'react';
-import LinkItem from './LinkItem';
 
 export default function Breadcrumb() {
 	const { segment } = usePageSegment();
@@ -16,27 +16,35 @@ export default function Breadcrumb() {
 	return (
 		<>
 			<li>
-				<LinkItem
+				<Link
 					aria-label='Go to homepage'
+					className={`link gap-1 capitalize ${
+						!Boolean(segment)
+							? 'no-animation pointer-events-none !cursor-default !no-underline'
+							: 'link-hover link-primary visited:link-accent'
+					}`}
 					href='/'
-					icon={<HomeIcon className='aspect-square h-4 !text-primary' />}
-					isActive={!segment}
 				>
+					<HomeIcon className='aspect-square h-4 !text-primary' />
 					<span className='hidden sm:inline'>home</span>
-				</LinkItem>
+				</Link>
 			</li>
 
 			<li className='gap-2 before:!m-0 2xl:gap-3'>
 				{moduleIdList.map((m, i) => (
 					<Fragment key={m}>
-						<LinkItem
-							aria-label={`Go to ${m} Search`}
+						<Link
+							aria-label={`Go to ${m} Page`}
+							className={`link gap-1 capitalize ${
+								m === segment
+									? 'no-animation pointer-events-none !cursor-default !no-underline'
+									: 'link-hover link-primary visited:link-accent'
+							}`}
 							href={`/${m}`}
-							icon={<LinkIcon className='hidden aspect-square h-4 2xl:inline' />}
-							isActive={m === segment}
 						>
+							<LinkIcon className='hidden aspect-square h-4 2xl:inline' />
 							{m}
-						</LinkItem>
+						</Link>
 
 						{i < moduleIdList.length - 1 ? (
 							<div className='aspect-square h-2/3 rounded-full bg-base-content/40 sm:h-1.5' />
@@ -46,14 +54,18 @@ export default function Breadcrumb() {
 			</li>
 
 			<li className='gap-2 before:!m-0 2xl:gap-3'>
-				<LinkItem
-					aria-label={`Go to about page`}
+				<Link
+					aria-label='Go to about page'
+					className={`link gap-1 capitalize ${
+						arrayInclude(listAboutPaths, segment)
+							? 'no-animation pointer-events-none !cursor-default !no-underline'
+							: 'link-hover link-primary visited:link-accent'
+					}`}
 					href='/about'
-					icon={<InfoIcon className='aspect-square h-4 !text-primary' />}
-					isActive={arrayInclude(listAboutPaths, segment)}
 				>
+					<InfoIcon className='aspect-square h-4 !text-primary' />
 					<span className='hidden sm:inline'>about</span>
-				</LinkItem>
+				</Link>
 			</li>
 		</>
 	);
