c88f5ad84c74805be86f94bedb58ebce567446e1
Pham Anh Dung <anhdungpham090@gmail.com>
2024-01-07 20:51:18 +0700

    fix upstash slow down the app.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
-import { APIError } from '@root/types/common';
-import { tryCatchHandler, writeLog } from '@root/utils/common';
-import { getIpAddress } from '@root/utils/server';
-import { ratelimit } from '@root/utils/server/ratelimit';
+// import { APIError } from '@root/types/common';
+// import { tryCatchHandler, writeLog } from '@root/utils/common';
+// import { getIpAddress } from '@root/utils/server';
+// import { ratelimit } from '@root/utils/server/ratelimit';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-	const ratelimitResult = await tryCatchHandler(ratelimit.limit(`IP_ADDRESS:${getIpAddress(request.headers)}`));
+	// const ratelimitResult = await tryCatchHandler(ratelimit.limit(`IP_ADDRESS:${getIpAddress(request.headers)}`));
 
-	if (!ratelimitResult.isSuccess) {
-		writeLog({ args: [`Error get ratelimit data:`, ratelimitResult.error], type: 'error' });
+	// if (!ratelimitResult.isSuccess) {
+	// 	writeLog({ args: [`Error get ratelimit data:`, ratelimitResult.error], type: 'error' });
 
-		return NextResponse.json({
-			data: null,
-			error: new APIError({ code: 'INTERNAL_SERVER_ERROR' }),
-			isSuccess: false,
-		});
-	}
+	// 	return NextResponse.json({
+	// 		data: null,
+	// 		error: new APIError({ code: 'INTERNAL_SERVER_ERROR' }),
+	// 		isSuccess: false,
+	// 	});
+	// }
 
-	if (!ratelimitResult.data.success)
-		return NextResponse.json({
-			data: null,
-			error: new APIError({ code: 'TOO_MANY_REQUESTS' }),
-			isSuccess: false,
-		});
+	// if (!ratelimitResult.data.success)
+	// 	return NextResponse.json({
+	// 		data: null,
+	// 		error: new APIError({ code: 'TOO_MANY_REQUESTS' }),
+	// 		isSuccess: false,
+	// 	});
 
 	return NextResponse.next();
 }
