b989000ba229ffbf2479bc1fbb49bea8512d9df0
Pham Anh Dung <anhdungpham090@gmail.com>
2024-04-11 09:31:07 +0700

    try move getServerSession to content and home layout group

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,10 @@
 import PageBanner from '@components/layout/static/PageBanner';
 import type { ChildrenProps } from '@root/types/common/props';
+import { getServerSession } from 'next-auth';
 
-export default function HomeLayout({ children }: ChildrenProps) {
+export default async function HomeLayout({ children }: ChildrenProps) {
+	const session = await getServerSession();
+
 	return (
 		<>
 			<PageBanner bannerType='full' />
