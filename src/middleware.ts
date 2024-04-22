b4d4950f43288fec6446c6344259b22d5c7162fd
Pham Anh Dung <anhdungpham090@gmail.com>
2024-04-22 15:08:52 +0700

    quick fix redis.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,28 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import type { APIResult } from './types/common';
-import { APIError } from './types/common';
-import { tryCatchHandler, writeLog } from './utils/common';
-import { getIpAddress } from './utils/server';
-import { ratelimit } from './utils/server/ratelimit';
 
 export async function middleware(request: NextRequest) {
-	const ipAddress = getIpAddress(request.headers);
+	// const ipAddress = getIpAddress(request.headers);
 
-	writeLog({ args: [`Request from IP: [${ipAddress}]`], type: 'log' });
+	// writeLog({ args: [`Request from IP: [${ipAddress}]`], type: 'log' });
 
-	const ratelimitResult = await tryCatchHandler(ratelimit.limit(ipAddress));
+	// const ratelimitResult = await tryCatchHandler(ratelimit.limit(ipAddress));
 
-	if (!ratelimitResult.isSuccess) {
-		return NextResponse.json({
-			result: null,
-			error: new APIError({ code: 'INTERNAL_SERVER_ERROR' }),
-			isSuccess: false,
-		} satisfies APIResult);
-	}
+	// if (!ratelimitResult.isSuccess) {
+	// 	return NextResponse.json({
+	// 		result: null,
+	// 		error: new APIError({ code: 'INTERNAL_SERVER_ERROR' }),
+	// 		isSuccess: false,
+	// 	} satisfies APIResult);
+	// }
 
-	if (!ratelimitResult.data.success) {
-		return NextResponse.json({
-			result: null,
-			error: new APIError({ code: 'TOO_MANY_REQUESTS' }),
-			isSuccess: false,
-		} satisfies APIResult);
-	}
+	// if (!ratelimitResult.data.success) {
+	// 	return NextResponse.json({
+	// 		result: null,
+	// 		error: new APIError({ code: 'TOO_MANY_REQUESTS' }),
+	// 		isSuccess: false,
+	// 	} satisfies APIResult);
+	// }
 
 	return NextResponse.next();
 }
